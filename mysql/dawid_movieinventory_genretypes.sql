d75a7db644686702b89650aa04a157352ac25e79
Dawid Kasperowicz <me@dawidk.ca>
2020-02-18 00:19:02 -0500

    Update mysql script

--- mysql/dawid_movieinventory_genretypes.sql
+++ mysql/dawid_movieinventory_genretypes.sql
@@ -30,6 +30,16 @@ CREATE TABLE `genretypes` (
   PRIMARY KEY (`genreID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genretypes`
+--
+
+LOCK TABLES `genretypes` WRITE;
+/*!40000 ALTER TABLE `genretypes` DISABLE KEYS */;
+INSERT INTO `genretypes` VALUES (0,'Action'),(1,'Adult'),(2,'Adventure'),(3,'Animation'),(4,'Comedy'),(5,'Crime'),(6,'Cult'),(8,'Documentary'),(9,'Drama'),(10,'Family'),(11,'Fantasy'),(12,'Gangster'),(13,'Historical'),(14,'Horror'),(15,'Medical'),(16,'Musical'),(17,'Paranormal'),(18,'Romance'),(19,'Science Fiction'),(20,'Sport'),(21,'Thriller'),(22,'War'),(23,'Western');
+/*!40000 ALTER TABLE `genretypes` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -40,4 +50,4 @@ CREATE TABLE `genretypes` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-17 23:16:01
+-- Dump completed on 2020-02-18  0:17:20
